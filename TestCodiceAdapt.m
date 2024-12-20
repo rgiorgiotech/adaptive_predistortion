0fc3e3d9a3cf611c77231a1a1f7c00e8a103566b
MG <matteogramignano@gmail.com>
2024-12-20 15:09:45 +0100

    Modifica codice 'TestCodiceAdapt'

--- TestCodiceAdapt.m
+++ TestCodiceAdapt.m
@@ -1,32 +1,23 @@
 %% Indicizzazione sequenza adattiva 
 
-x = zeros(0,10);
-for k = 1:10
+L = input('Enter a positive integer number. ');
+x = zeros(0,L);
+for k = 1:L
     x(k) = randn(1);
 end
-x = x / max(x);
+x = x / max(abs(x)); 
+% La sequenza viene normalizzata rispetto al valore massimo (in valore assoluto),
+% a simulare l'effetto della saturazione (-1 <= x <= 1).
 
 N = 16;
 gamma = 0.4;
 xmax = 1;
 K = sqrt(.997);
-delta = abs(xmax)/(2*N);
+delta = xmax/(2*N);
 
 if abs(x) > xmax
     n = N;
 else
     n = (abs(x) - (mod(abs(x), delta))) / delta;
 end
-
-% n_cresc = zeros(size(n));
-% for i = 1:size(n)
-%     for j = 1:size(n)
-%         k = i;
-%         while n(k) < n(j) 
-%             k = k + 1;
-%         end
-%         n_cresc(i) = n(j);
-%     end
-% end
-% n_cresc
 
